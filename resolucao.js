5fe29b475e3318b529df4c556bc41559591f3896
RafaelLaporte <rafanex@gmail.com>
2020-08-22 13:17:02 -0300

    The functions were refactored

--- resolucao.js
+++ resolucao.js
@@ -1,21 +1,8 @@
 const fs = require('fs');
 const file = 'raw.txt'
 
-
-//Executes the code
-let data = dataReader(file);
-data = correctNames(data);
-correctPrices(data);
-correctQuantities(data);
-
-const correctedFile = correctData(data);
-const correctedData = dataReader(correctedFile);
-
-sortData(correctedData);
-stockValue(correctedData);
-
 //Function 1 - Read file
-function dataReader(file) {
+function readFromFile(file) {
     try {
         var data = fs.readFileSync(file, 'utf-8');
     } catch (err) {
@@ -26,51 +13,46 @@ function dataReader(file) {
 };
 
 //Function 2 - Corrects the wrong caracters 
-function correctNames(data) {
+function fixData(data) {
 
-    dataString = JSON.stringify(data);
+    let dataString = JSON.stringify(data);
 
-    correctChars = {
+    charModel = {
         "æ" : 'a',
         "¢" : 'c',
         "ø" : 'o',
         "ß" : 'b'
     };
  
-    for (letter of dataString) {
-        let keyList = Object.keys(correctChars);
+    for (char of dataString) {
+        let keyList = Object.keys(charModel);
  
-        if (keyList.includes(letter)) {
-            let regex = new RegExp(letter, "g");
-            var dataString = dataString.replace(regex, correctChars[letter]);
+        if (keyList.includes(char)) {
+            let regex = new RegExp(char, "g");
+            dataString = dataString.replace(regex, charModel[char]);
         };
     };
 
-    return JSON.parse(dataString);
+    Object.assign(data, JSON.parse(dataString));
 };
 
-//Function 3 - Corrects the prices
-function correctPrices(data) {
+//Function 3 - Fix the prices
+function fixPrices(data) {
     data.forEach(product => {
         if (!isNaN(product.price)) product.price = Number(product.price);
     });
-    //console.log(data);
 };
 
-//Function 4 - Correct quantities
-function correctQuantities(data) {
+//Function 4 - Fix the quantities
+function fixQuantities(data) {
     data.forEach(product => {
-        if (product.quantity == undefined) product.quantity = 0;
-    })
-    //console.log(data);
+        product.quantity = !product.quantity ? 0 : product.quantity
+    });
 };
 
 //Function 5 - Export the JSON file corrected data.
-function correctData(data) {
-    let fileName = 'saida.json';
+function exportDataToFile(data, fileName) {
     fs.writeFileSync(fileName, JSON.stringify(data, null, 4));
-
-    return fileName
 };
 
 //Validation Functions
@@ -128,7 +110,8 @@ function stockValue(file) {
             let quantity = product.quantity;
             let price = product.price;
 
-            if (categoryValue[categoryName] == undefined) categoryValue[categoryName] = 0;
+            //if (categoryValue[categoryName] == undefined) categoryValue[categoryName] = 0;
+            categoryValue[categoryName] = !categoryValue[categoryName] ? 0 : categoryValue[categoryName]
        
             categoryValue[categoryName] += quantity*price;
         });        
@@ -139,3 +122,13 @@ function stockValue(file) {
     console.log(`Total value in stock by category:`);
     console.log(categoryValue);
 }
+
+//Executes the code
+let data = readFromFile(file);
+fixData(data);
+fixPrices(data);
+fixQuantities(data);
+exportDataToFile(data, 'saida.json');
+
+sortData(data);
+stockValue(data);
